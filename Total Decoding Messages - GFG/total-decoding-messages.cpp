3cfad0e79f077168d68ee96e1dca01f477cd301a
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-04 20:31:16 +0530

    Added solution - LeetHub

---
+++ Total Decoding Messages - GFG/total-decoding-messages.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		int CountWays(string str){
+		    int n =str.size();
+int dp[n];
+dp[0]=1;
+
+if(str[0]=='0')
+return 0;
+
+for(int i =1;i<n;i++) {
+    if(str[i-1]=='0' && str[i]=='0') {
+        dp[i]=0;
+        }
+        else if(str[i-1]=='0' && str[i]!='0' ){
+            dp[i]=dp[i-1]; }
+            else if(str[i-1]!='0' && str[i]=='0'){
+                if(str[i-1]=='1'||str[i-1]=='2'){
+                    dp[i]=(i>=2?dp[i-2]:1);
+                    
+                } else
+                dp[i] =0;
+}
+else if(str[i-1]!='0' and str[i]!='0')
+{
+if(stoi(str.substr(i-1,2))<=26)
+dp[i] = dp[i-1]+(i>=2?dp[i-2]:1);
+else
+dp[i] = dp[i-1];
+}
+dp[i]=dp[i]%1000000007;
+}
+
+return dp[n-1];
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.CountWays(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
