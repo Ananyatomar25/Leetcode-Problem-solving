edbff1cd8922bd46a67aa2ab1e03f62443f2db0a
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-06 23:54:41 +0530

    Time: 20 ms (61.02%), Space: 18.5 MB (53.01%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int longestMountain(vector<int>& A) {
-        if(A.size()==0) return 0;
-        int len=0;
-        int ma=0,flag=0;
-        for(int i=0;i<A.size()-1;){
-            if(A[i]>A[i+1]){
-                flag=1;
-                int c=1;
-                while(i<A.size()-1 && A[i]>A[i+1]){
-                    c++;
-                    i++;
+    int longestMountain(vector<int>& arr) {
+        int ans =0;
+        int n = arr.size();
+        
+        for(int i=1;i<=n-2;){
+            if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+                
+                int count = 1;
+                int j =i;
+                while(j>0 && arr[j]>arr[j-1]){
+                    j--;
+                    count++;
                 }
-                if(c!=0  && len!=0){
-                ma=max(ma,len+c);}
-                len=0;
+                while(i<n-1 && arr[i]>arr[i+1]){
+                    i++;
+                    count++;
+                }
+                
+                ans = max(ans,count);
             }
-            else if(A[i]<A[i+1]){
-                flag=1;
-                i++;
-                len++;
-            }
+            
             else{
-                len=0;
                 i++;
             }
         }
-        if(!flag) return 0;
-        return ma;
+        
+        return ans;
     }
 };
