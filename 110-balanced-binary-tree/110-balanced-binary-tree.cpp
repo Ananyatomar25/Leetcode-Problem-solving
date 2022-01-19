f8f4aeee06b4fedde123b2bca2ab5c2bc6811d93
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-20 00:01:44 +0530

    Time: 4 ms (99.26%), Space: 21.1 MB (22.74%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,31 +11,59 @@
  */
 class Solution {
 public:
-     int Lheight(TreeNode* node){
-         if(node == NULL) return 0;
-         else return (1+max(Lheight(node->left), Lheight(node->right)));
-     }
-    int Rheight(TreeNode* node){
-         if(node == NULL) return 0;
-         else return (1+max(Rheight(node->left), Rheight(node->right)));
-     }
+    
+    int height(TreeNode* node){
+        if(node==NULL) return 0;
+        
+        int lh = height(node->left);
+        if(lh == -1) return -1;
+        int rh = height(node->right);
+        if(rh == -1) return -1;
+        if(abs(lh-rh)>1) return -1;
+        return (1+max(lh,rh));
+        
+    }
+    
+    ///---takes O(N^2) to compute lh and rh
+    
+    
+    //  int Lheight(TreeNode* node){
+    //      if(node == NULL) return 0;
+    //      else return (1+max(Lheight(node->left), Lheight(node->right)));
+    //  }
+    // int Rheight(TreeNode* node){
+    //      if(node == NULL) return 0;
+    //      else return (1+max(Rheight(node->left), Rheight(node->right)));
+    //  }
     
     
     
     
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
+//         if(root==NULL) return true;
         
-        int l = Lheight(root->left);
-        int r = Rheight(root->right);
+//         int l = Lheight(root->left);
+//         int r = Rheight(root->right);
         
-        if(abs(l-r)>1) return false;
+//         if(abs(l-r)>1) return false;
         
-        bool ll = isBalanced(root->left);
-        bool rr = isBalanced(root->right);
+//         bool ll = isBalanced(root->left);
+//         bool rr = isBalanced(root->right);
         
-        if(ll==false || rr == false) return false;
+//         if(ll==false || rr == false) return false;
         
-        return true;
+//         return true;
+        
+        
+        
+        
+        
+        // O(N) solution
+        
+        
+        return (height(root)!= -1);
+        
+        
+        
     }
 };
