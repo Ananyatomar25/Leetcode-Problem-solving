6803b79d0107a4e889969575ae5ff54da3647e1f
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-25 20:44:33 +0530

    Time: 42 ms (33.74%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -5,29 +5,37 @@ public:
         if(arr.size()<3) return false;
         
         int n = arr.size();
-        int i=1;
-        bool flag = false;
+//         int i=1;
+//         bool flag = false;
         
-        while(i<n-1){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                flag = true;
-                int j =i;
-                while(j>0){
-                    if(arr[j-1]<arr[j])
-                        j--;
-                    else return false;
-                }
-                int k =i;
-                while(k<n-1){
-                    if(arr[k]>arr[k+1])
-                        k++;
-                    else return false;
-                }
-            }
-            i++;
-        }
+//         while(i<n-1){
+//             if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+//                 flag = true;
+//                 int j =i;
+//                 while(j>0){
+//                     if(arr[j-1]<arr[j])
+//                         j--;
+//                     else return false;
+//                 }
+//                 int k =i;
+//                 while(k<n-1){
+//                     if(arr[k]>arr[k+1])
+//                         k++;
+//                     else return false;
+//                 }
+//             }
+//             i++;
+//         }
         
-        if(flag == false) return false;
-        return true;
+//         if(flag == false) return false;
+//         return true;
+        
+        
+        int i = 0, j = n-1;
+        
+        while(i+1<n-1 && arr[i]<arr[i+1]) i++;
+        while(j-1 >0 && arr[j]<arr[j-1]) j--;
+        
+        return i==j;
     }
 };
