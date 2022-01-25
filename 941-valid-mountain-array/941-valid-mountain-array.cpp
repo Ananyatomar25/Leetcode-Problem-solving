d009615c5d5dafe8f9dad6ace80b40618f05ab22
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-25 13:06:14 +0530

    Time: 52 ms (19.90%), Space: 22.5 MB (48.39%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        
+        if(arr.size()<3) return false;
+        
+        int n = arr.size();
+        int i=1;
+        bool flag = false;
+        
+        while(i<n-1){
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                flag = true;
+                int j =i;
+                while(j>0){
+                    if(arr[j-1]<arr[j])
+                        j--;
+                    else return false;
+                }
+                int k =i;
+                while(k<n-1){
+                    if(arr[k]>arr[k+1])
+                        k++;
+                    else return false;
+                }
+            }
+            i++;
+        }
+        
+        if(flag == false) return false;
+        return true;
+    }
+};
