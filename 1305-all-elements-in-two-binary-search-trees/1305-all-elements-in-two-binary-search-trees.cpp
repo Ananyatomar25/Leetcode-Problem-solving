399fd22cd85566e1dcfbaafe5dd1997e4914a496
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-26 20:30:36 +0530

    Time: 144 ms (59.97%), Space: 84.3 MB (97.28%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -12,51 +12,52 @@
 class Solution {
 public:
     
-    void inorderOfFirst(TreeNode* node1,vector<int>& in1){
+    void inorder(TreeNode* node1,vector<int>& ans){
         if(node1==NULL) return;
-        inorderOfFirst(node1->left,in1);
-        in1.push_back(node1->val);
-        inorderOfFirst(node1->right,in1);
+        inorder(node1->left,ans);
+        ans.push_back(node1->val);
+        inorder(node1->right,ans);
     }
     
-    void inorderOfSecond(TreeNode* node2,vector<int>& in2){
-        if(node2==NULL) return;
-        inorderOfSecond(node2->left,in2);
-        in2.push_back(node2->val);
-        inorderOfSecond(node2->right,in2);
-    }
+//     void inorderOfSecond(TreeNode* node2,vector<int>& in2){
+//         if(node2==NULL) return;
+//         inorderOfSecond(node2->left,in2);
+//         in2.push_back(node2->val);
+//         inorderOfSecond(node2->right,in2);
+//     }
     
     
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         vector<int> ans;
-        if(root1 == NULL && root2 == NULL) return ans;
-        vector<int> in1;
-        vector<int> in2;
+        // if(root1 == NULL && root2 == NULL) return ans;
+        // vector<int> in1;
+        // vector<int> in2;
        
         
-        inorderOfFirst(root1,in1);
-        inorderOfSecond(root2,in2);
-        if(root1 == NULL)return in2;
-        if(root2 ==NULL) return in1;
+        inorder(root1,ans);
+        inorder(root2,ans);
+//         if(root1 == NULL)return in2;
+//         if(root2 ==NULL) return in1;
         
-        int n1 = in1.size();
-        int n2 = in2.size();
+//         int n1 = in1.size();
+//         int n2 = in2.size();
          
-        int i=0,j=0;
+//         int i=0,j=0;
         
-        while(i<n1 && j<n2){
-            if(in1[i]<in2[j]){
-                ans.push_back(in1[i]);
-                i++;
-            }
-            else if(in1[i]>=in2[j]){
-                ans.push_back(in2[j]);
-                j++;
-            }
-        }
+//         while(i<n1 && j<n2){
+//             if(in1[i]<in2[j]){
+//                 ans.push_back(in1[i]);
+//                 i++;
+//             }
+//             else if(in1[i]>=in2[j]){
+//                 ans.push_back(in2[j]);
+//                 j++;
+//             }
+//         }
         
-        for(;i<n1;i++) ans.push_back(in1[i]);
-        for(;j<n2;j++) ans.push_back(in2[j]);
+//         for(;i<n1;i++) ans.push_back(in1[i]);
+//         for(;j<n2;j++) ans.push_back(in2[j]);
+        sort(ans.begin(),ans.end());
         
         return ans;
         
