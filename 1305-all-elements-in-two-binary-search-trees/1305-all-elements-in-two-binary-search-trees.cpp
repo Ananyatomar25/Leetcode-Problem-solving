2212c62a8307c987c335f84bfc08568d8ee235ab
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-01-27 19:54:50 +0530

    Time: 300 ms (9.24%), Space: 84.5 MB (67.13%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,58 +11,23 @@
  */
 class Solution {
 public:
-    
-    void inorder(TreeNode* node1,vector<int>& ans){
-        if(node1==NULL) return;
-        inorder(node1->left,ans);
-        ans.push_back(node1->val);
-        inorder(node1->right,ans);
-    }
-    
-//     void inorderOfSecond(TreeNode* node2,vector<int>& in2){
-//         if(node2==NULL) return;
-//         inorderOfSecond(node2->left,in2);
-//         in2.push_back(node2->val);
-//         inorderOfSecond(node2->right,in2);
-//     }
-    
+    void pushLeft(stack<TreeNode*> &s, TreeNode* n) {
+    while (n != nullptr) 
+        s.push(exchange(n, n->left));
+}
     
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        // if(root1 == NULL && root2 == NULL) return ans;
-        // vector<int> in1;
-        // vector<int> in2;
-       
-        
-        inorder(root1,ans);
-        inorder(root2,ans);
-//         if(root1 == NULL)return in2;
-//         if(root2 ==NULL) return in1;
-        
-//         int n1 = in1.size();
-//         int n2 = in2.size();
-         
-//         int i=0,j=0;
-        
-//         while(i<n1 && j<n2){
-//             if(in1[i]<in2[j]){
-//                 ans.push_back(in1[i]);
-//                 i++;
-//             }
-//             else if(in1[i]>=in2[j]){
-//                 ans.push_back(in2[j]);
-//                 j++;
-//             }
-//         }
-        
-//         for(;i<n1;i++) ans.push_back(in1[i]);
-//         for(;j<n2;j++) ans.push_back(in2[j]);
-        sort(ans.begin(),ans.end());
-        
-        return ans;
-        
-        
-        
-        
+         vector<int> res;
+    stack<TreeNode*> s1, s2;
+    pushLeft(s1, root1);
+    pushLeft(s2, root2);
+    while (!s1.empty() || !s2.empty()) {
+        stack<TreeNode*> &s = s1.empty() ? s2 : s2.empty() ? s1 : 
+            s1.top()->val < s2.top()->val ? s1 : s2;
+        auto n = s.top(); s.pop();
+        res.push_back(n->val);
+        pushLeft(s, n->right);
+    }
+    return res;
     }
 };
