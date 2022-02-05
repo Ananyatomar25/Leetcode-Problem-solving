76dbecd274d1416f3abc6584d371b9764b50f37b
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-02-06 00:38:11 +0530

    Time: 38 ms (48.77%), Space: 13.4 MB (58.51%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,29 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+class mycompare{
+    public:
+    bool operator()(const ListNode* a, const ListNode* b){
+        return (a->val > b->val);
+    }
+};
+
+
+
 class Solution {
 public:
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> temp;
+        
         ListNode* head = new ListNode(0);
-        ListNode* t = head;
+        ListNode* temp = head;
+        priority_queue<ListNode*, vector<ListNode*>,mycompare> pq;
+        
         for(int i=0;i<lists.size();i++){
-            ListNode* node= lists[i];
-            while(node!=NULL){
-                temp.push_back(node->val);
-                node = node->next;
-            }
+            if(lists[i]!=NULL)
+                pq.push(lists[i]);
+        }
+        
+        while(!pq.empty()){
+            ListNode* node = pq.top();
+            pq.pop();
+            temp->next = node;
+            temp = node;
+            if(node->next) pq.push(node->next);
         }
         
         
-        sort(temp.begin(),temp.end());
-        for(auto it:temp){
-            ListNode* n = new ListNode(it);
-            t->next = n;
-            t = n;
-        }
-        
-        return head->next;
+    return head->next;
         
         
     }
