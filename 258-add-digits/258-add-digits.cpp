786c680cd6ce85ccf9e0a7ca5f6c4feb983c2fde
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-02-08 22:25:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -3,17 +3,20 @@ public:
     int addDigits(int num) {
         
         if(num == 0) return 0;
-        int ans =0;
+//         int ans =0;
         
-       string temp = to_string(num);
-        while(temp.length()>=1){
-            int sum =0;
-            if(temp.length() == 1) break;
-            for(int i=0;i<temp.length();i++){
-                sum = sum+(temp[i]-'0');
-            }
-            temp = to_string(sum);
-        }
-        return (ans=stoi(temp));
+//        string temp = to_string(num);
+//         while(temp.length()>=1){
+//             int sum =0;
+//             if(temp.length() == 1) break;
+//             for(int i=0;i<temp.length();i++){
+//                 sum = sum+(temp[i]-'0');
+//             }
+//             temp = to_string(sum);
+//         }
+//         return (ans=stoi(temp));
+        
+        if(num%9 == 0) return 9;
+        else return num%9;
     }
 };
