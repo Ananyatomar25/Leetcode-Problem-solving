fe020fffca62f63ae7275aac112297569e3e1218
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-02-09 22:57:47 +0530

    Time: 12 ms (94.63%), Space: 11.2 MB (82.51%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -3,22 +3,45 @@ public:
     int findPairs(vector<int>& nums, int k) {
         int count =0;
         
-        unordered_map<int,int> mp;
+//         unordered_map<int,int> mp;
         
-        for(auto it: nums){
-            mp[it]++;
-        }
-        for(auto it: mp){
-            if(k==0){
-                if(it.second>1) count++;
-            }
+//         for(auto it: nums){
+//             mp[it]++;
+//         }
+//         for(auto it: mp){
+//             if(k==0){
+//                 if(it.second>1) count++;
+//             }
             
-            else if(mp.find(it.first+k) != mp.end()){
-                 count++;
-            }
-        }
+//             else if(mp.find(it.first+k) != mp.end()){
+//                  count++;
+//             }
+//         }
         
-        return count;
+//         return count;
+        sort(nums.begin(),nums.end());
+ 		int j=1;
+ 		int i=0;
+ 		
+ 		while(i<nums.size() && j<nums.size()){
+ 			if(nums[j]-nums[i]==k){
+ 				count++;
+ 				j++;
+ 				i++;
+ 				while(j<nums.size() && nums[j]==nums[j-1]){
+ 					j++;
+ 				}
+ 			}
+ 			else if(nums[j]-nums[i]>k){
+ 				i++;  
+ 				if(j-i==0){
+ 					j++;}
+ 			}
+ 			else{      
+ 				j++;
+ 			}         
+		}
+ 		return count;
         
     }
 };
