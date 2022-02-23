a4faa19e7b961462787a70f0d09de83ee7699d40
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-02-23 23:56:30 +0530

    Time: 11 ms (35.55%), Space: 9.2 MB (9.88%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    Node* dfs(Node* curr, unordered_map<Node*, Node*> &mpp){
+        
+        vector<Node*> neighbor;
+        Node* clone = new Node(curr->val);
+        mpp[curr] = clone;
+        
+        for(auto it: curr->neighbors){
+            if(mpp.find(it) != mpp.end()){
+                neighbor.push_back(mpp[it]);
+            }
+            else{
+                neighbor.push_back(dfs(it,mpp));
+            }
+        }
+        
+        clone->neighbors = neighbor;
+        
+        return clone;
+    }
+    
+    Node* cloneGraph(Node* node) {
+        
+        unordered_map<Node*, Node*> mpp;
+        
+        if(node == NULL) return NULL;
+        
+        if(node->neighbors.size() == 0){
+            Node* newNode = new Node(node->val);
+            return newNode;
+        }
+        
+        Node* result = dfs(node, mpp);
+        return result;
+     
+    }
+};
