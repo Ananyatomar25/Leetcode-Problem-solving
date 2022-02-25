2650818594b9d916fa4edfd863e815d647aadb22
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-02-26 01:52:01 +0530

    Time: 5 ms (16.36%), Space: 6.1 MB (73.21%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+        int i=0,j=0, m = version1.length(), n= version2.length();
+        
+        while(i<m || j<n){
+            int n1 =0,n2=0;
+            
+            while(i<m && version1[i] != '.'){
+                n1 = n1*10 + (version1[i] -'0');
+                i++;
+            }
+            while(j<n && version2[j] != '.'){
+                n2 = n2*10 + (version2[j] -'0');
+                j++;
+            }
+            
+            if(n1 < n2) return -1;
+            else if(n1 > n2) return 1;
+            i++;
+            j++;
+            
+        }
+        
+        return 0;
+        
+    }
+};
