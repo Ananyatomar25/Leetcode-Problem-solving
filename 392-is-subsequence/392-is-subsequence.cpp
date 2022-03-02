b22d6edd2455df8671a1885b5daa57b4982add2c
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-03-03 01:31:51 +0530

    Time: 3 ms (47.08%), Space: 6.8 MB (19.41%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         
-        if(s.size() == 0) return true;
         
-         
-        int i=0,j=0;
+        unordered_map<char, vector<int>> mp;
         
-        while(i< s.size() && j< t.size()){
-            if(s[i] == t[j]){
-                i++;
-                j++;
-            }
-            else if(s[i] != t[j]) j++;
+        for(int i=0;i<t.length();i++){
+            mp[t[i]].push_back(i);
         }
+        int prev =-1;
+        for(auto c: s){
+            auto it = mp.find(c);
+            if(it == mp.end()) return false;
+            auto v = it->second;
+            int pos = upper_bound(v.begin(),v.end(),prev) - v.begin();
+            if(pos == v.size()) return false;
+            prev = v[pos];
+        }
+        return true;
         
-        if(i == s.size()) return true;
-        return false;
+        
+//         if(s.size() == 0) return true;  // empty string is always a subsequence
+         
+//         int i=0,j=0;
+        
+//         while(i< s.size() && j< t.size()){
+//             if(s[i] == t[j]){
+//                 i++;
+//                 j++;
+//             }
+//             else if(s[i] != t[j]) j++;
+//         }
+        
+//         if(i == s.size()) return true;
+//         return false;
         
     }
 };
