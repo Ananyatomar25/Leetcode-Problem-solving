20d23d346ece5171be048df2d8dcb72ee6b8685a
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-03-06 01:26:26 +0530

    Time: 4 ms (94.58%), Space: 9.1 MB (97.48%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        
+        int freq[10002] ={0};
+        int dp[10002] ={0};
+        
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        
+        dp[0] =0;
+        dp[1] = 1*freq[1];
+        
+        for(int i=2;i<10002;i++){
+            dp[i] = max(dp[i-1], dp[i-2] + i*freq[i]);
+        }
+        
+        return dp[10000];
+        
+    }
+};
