a8b1838d026b7630895552b87d1e4de2604897aa
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-07-14 16:44:37 +0530

    Time: 30 ms (67.29%), Space: 16.4 MB (31.27%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,34 +1,53 @@
 class MinStack {
 public:
-    stack <pair < int, int >> st;
+    
+    stack < long long > st;
+    long long mini;
+    
     MinStack() {
-        
-        
+      while (st.empty() == false) st.pop();
+      mini = INT_MAX;
     }
     
-    void push(int val) {
-        if(st.empty()) st.push({val,val});
-        else if(val < st.top().second){
-            st.push({val,val});
+    void push(int value) {
+        long long val = (long long)value;
+        if(st.empty()){
+            mini = val;
+            st.push(val);
         }
-        else {
-            st.push({val, st.top().second});
+        
+        else{
+            if(val < mini){
+                long long modified = (2*val) - mini;
+                mini = val;
+                st.push(modified);
+            }
+            else st.push(val);
         }
     }
     
     void pop() {
-        pair<int, int> temp = st.top();
+        if (st.empty()) return;
+        long long el = st.top();
         st.pop();
+
+    if (el < mini) {
+      mini = 2 * mini - el;
+    }
     }
     
     int top() {
-        int res = st.top().first;
-        return res;
+        if (st.empty()) return -1;
+        
+        else if(st.top() < mini){
+            return mini;
+        }
+        return st.top();
+        
     }
     
     int getMin() {
-        int res = st.top().second;
-        return res;
+        return mini;
     }
 };
 
