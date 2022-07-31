277ccddf8e90c66a7b8f384ba616d79c84aca7f9
Ananya Tomar <71659812+Ananyatomar25@users.noreply.github.com>
2022-08-01 00:54:11 +0530

    Time: 301 ms (33.41%), Space: 91 MB (33.39%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,43 +1,24 @@
-class Solution {
-public:
-    
-    int f(int ind, vector<int> &prices, int buy, int fee, vector<vector<int>> &dp){
-        int n = prices.size();
-        if(ind == n) return 0;
-        int profit = 0;
-        
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        
-        if(buy == 1){
-            profit = max((-prices[ind] + f(ind+1,prices,0,fee,dp)), f(ind+1, prices, 1, fee,dp));
-        }
-        else{
-            profit = max((prices[ind] - fee + f(ind+1,prices,1,fee,dp)), f(ind+1, prices, 0, fee,dp));
-        }
-        
-        return dp[ind][buy] = profit;
-    }
-    
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        int profit = 0;
-        
-        dp[n][0] = 0;
-        dp[n][1] = 0;
-        
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy = 0;buy<=1;buy++){
-                if(buy == 1){
-            profit = max((-prices[ind] + dp[ind+1][0]), dp[ind+1][1]);
-        }
-        else{
-            profit = max((prices[ind] - fee + dp[ind+1][1]), dp[ind+1][0]);
-        }
-                dp[ind][buy] = profit;
+class Solution
+{
+    public:
+
+        int maxProfit(vector<int> &prices, int fee)
+        {
+            int n = prices.size();
+            vector<vector < int>> dp(n + 1, vector<int> (2, 0));
+            int profit = 0;
+
+            dp[n][0] = 0;
+            dp[n][1] = 0;
+
+            for (int ind = n - 1; ind >= 0; ind--)
+            {
+
+                dp[ind][1] = max((-prices[ind] + dp[ind + 1][0]), dp[ind + 1][1]);
+
+                dp[ind][0] = max((prices[ind] - fee + dp[ind + 1][1]), dp[ind + 1][0]);
             }
+
+            return dp[0][1];
         }
-        
-        return dp[0][1];
-    }
 };
